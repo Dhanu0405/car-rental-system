9229225c3390c852b3b513c9083edb55fdc890eb
Dhanush Rajesh <dhanushr0405@gmail.com>
2025-04-20 21:20:47 +0530

    mongodb atlas

--- api/db/connect.js
+++ api/db/connect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const DB = 'mongodb://localhost:27017/vehicleRentalDB';
+// const DB = 'mongodb://localhost:27017/vehicleRentalDB';
+const DB = 'mongodb+srv://dhanush:Dhanu@0405r@cluster0.i0osx.mongodb.net/vehicleRentalDB?retryWrites=true&w=majority';
 mongoose.connect(DB).then(
     ()=>{
         console.log("DB connected...")
