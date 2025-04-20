e5a4e28ba15d9e807493db2f29208466d5cc36f3
Dhanush Rajesh <dhanushr0405@gmail.com>
2025-04-20 20:57:25 +0530

    changed some files

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin:"http://localhost:5173",
     credentials: true,
 }))
 
