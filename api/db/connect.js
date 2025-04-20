3b462dc8a93ee6208bd5daf035a06086245c8b25
Dhanush Rajesh <dhanushr0405@gmail.com>
2025-04-20 22:21:46 +0530

    mongodb connection string changes

--- api/db/connect.js
+++ api/db/connect.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-// const DB = 'mongodb://localhost:27017/vehicleRentalDB';
-const DB = 'mongodb+srv://dhanush:Dhanu@0405r@cluster0.i0osx.mongodb.net/vehicleRentalDB?retryWrites=true&w=majority';
+const DB = 'mongodb://localhost:27017/vehicleRentalDB';
 mongoose.connect(DB).then(
     ()=>{
         console.log("DB connected...")
